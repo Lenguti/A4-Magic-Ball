dd004eee74fe73f0133a4ffb024f8cbee9330c88
Lenard <lenard@buzzer.com>
2022-04-04 23:26:10 -0400

    add functionality

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -1,10 +1,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
+    @IBOutlet weak var magicBallImage: UIImageView!
+    @IBOutlet weak var revealAnswerButton: UIButton!
+    let ballArray:[UIImage?] = [
+        UIImage(named: "ball1"),
+        UIImage(named: "ball2"),
+        UIImage(named: "ball3"),
+        UIImage(named: "ball4"),
+        UIImage(named: "ball5")
+    ]
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+           
+        // Set snake eye label to not active.
+        magicBallImage.isHidden = true
+           
+        // Set Image Outlets to a UIImage with the given name.
+        revealAnswerButton.backgroundColor = .clear
+        revealAnswerButton.layer.cornerRadius = 5
+        revealAnswerButton.layer.borderWidth = 1
+        revealAnswerButton.layer.borderColor = UIColor.black.cgColor
+    }
 
-
+    @IBAction func revealAnswerTapped(_ sender: UIButton) {
+        print("reveal answer tapped")
+        magicBallImage.isHidden = false
+        magicBallImage.image = ballArray[Int.random(in: 0...4)]
+    }
 }
 
